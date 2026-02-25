f5d04338682ed438deb4d1387c1389af68f70d9f
venkatreddy142006-ops <venkatreddy142006@gmail.com>
2026-02-25 16:13:33 +0530

    Delete CRT/python directory

--- CRT/python/Assignments/M01_Language_&_Programming_Basics/AST04/task.py
+++
@@ -1,7 +0,0 @@
-def Reverse_String(s: str) -> str:
-   pass
-
-
-if __name__ == '__main__':
-    s = input()
-    print(Reverse_String(s))
--- CRT/python/Assignments/M01_Language_&_Programming_Basics/AST05/task.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-def Collatz_Sequence(n: int)-> List:
-   pass
-if __name__ == '__main__':
-    n = int(input())
-    print(Collatz_Sequence(n))
